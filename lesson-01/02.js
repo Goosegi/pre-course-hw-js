72bde11c3e02e2e87bdcb58f864751c8011eb51c
Artyom Serov <helloadobe2@gmail.com>
2024-05-21 15:23:50 +0300

    do lesson with ++ and --

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,3 +5,11 @@ let number1 = Math.floor(Math.random() * 100)
     } else {
         console.log(number1, ':Это нечетное число');
     }
+
+let number = 1;
+
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
+
