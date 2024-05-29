016d29d0ff5c80c3824961f9bebd5a3617f3d035
Artyom Serov <helloadobe2@gmail.com>
2024-05-29 14:03:52 +0300

    create second object

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -3,9 +3,18 @@ let passportWithAddress = {
     surname: "Petrov",
     address: {
         country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress1 = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
         city: "Bobryisk"
     }
 };
 
-console.log(passportWithAddress);
 console.log(passportWithAddress.address.city);
+console.log(passportWithAddress1.address.city);
