c0c36fbf63c92c3359974c36f3792fb776827991
Artyom Serov <helloadobe2@gmail.com>
2024-05-29 14:00:20 +0300

    edit city

---
+++ lesson-02/06.js
@@ -0,0 +1,11 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+console.log(passportWithAddress);
+console.log(passportWithAddress.address.city);
